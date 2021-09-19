7bd2a1e8a14292c6c0e534deaaea2adc8e7dd513
olukade01 <olukademuzzammil@gmail.com>
2021-09-20 00:09:53 +0100

    first commit

---
+++ 01_Introduction/datatypes.js
@@ -0,0 +1,14 @@
+let myName = "muzzammil";
+let isMarried = true;
+let command
+let octoberFirst = null;
+
+console.log (typeof myName); 
+console.log (typeof isMarried); 
+console.log (typeof command); 
+console.log (typeof octoberFirst); 
+
+let myAge = 25, yourAge = 30;
+
+console.log ("I am " + myAge + " years old.");
+console.log ("You are " + yourAge + " years old.");
---
+++ 01_Introduction/variable.js
@@ -0,0 +1,19 @@
+let myName = "muzzammil";
+let isMarried = true;
+let command
+let octoberFirst = null;
+
+let myCountry
+let myState
+let myGender
+let myAge
+
+let myAge = 10, ourAge = 20, myCountry = "nigeria", myGender = "male";
+
+let firstName = "olukade";
+let lastName = "muzamil";
+let maritalStatus = "single";
+let age = 02;
+let country = "canada";
+
+let firstName = "olukade", lastName = "muzamil", maritalStatus = "single", age = 02, country = "canada";
